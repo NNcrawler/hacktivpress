17397a1db8a8739df77804dc68a0a782726f64f2
NNcrawler <rasyid.xyz@gmail.com>
2017-11-09 17:04:23 +0700

    reappearing ALL nav, bug in nav show

--- client/src/components/nav.vue
+++ client/src/components/nav.vue
@@ -4,8 +4,8 @@
     <router-link :to="{ name: 'recent-post', params: {} }">Home</router-link>
     <router-link v-if="!isLogin" :to="{ name: 'register', params: {} }">register</router-link>
     <router-link v-if="!isLogin" :to="{ name: 'login', params: {} }">login</router-link>
-    <router-link :to="{ name: 'post', params: {} }">post</router-link>
-    <button v-if="isLogin" @click="logout" class="btn btn-link" type="button" name="button">logout</button>
+    <router-link v-if="!isLogin" :to="{ name: 'post', params: {} }">post</router-link>
+    <button v-if="!isLogin" @click="logout" class="btn btn-link" type="button" name="button">logout</button>
   </div>
 </template>
 
@@ -16,7 +16,6 @@ export default {
       alert('masu');
       this.$auth.signOut()
        .then(() => {
-         alert('masu');
          this.$store.commit('changeUser');
        });
     },
